6854725ccf657009d3df6d5017a28e9dd1f1d59d
daniel.qiu <daniel.qiu@networld.com>
2017-05-22 16:45:59 +0800

    revert

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 SEPARATOR = ";"
 
 LANG            = "en_US" # can be en_US, fr_FR, ...
-ANDROID_ID      = "0121eaa5c15995e5" # "xxxxxxxxxxxxxxxx"
+ANDROID_ID      = "3f0c2b01b168eb16" # "xxxxxxxxxxxxxxxx"
 GOOGLE_LOGIN    = "testerprice@gmail.com" # "username@gmail.com"
 GOOGLE_PASSWORD = "nwDaniel86!"
 AUTH_TOKEN      = None # "yyyyyyyyy"
